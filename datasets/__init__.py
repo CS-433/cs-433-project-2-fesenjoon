f9d5fa32dead16ca0da4c9e809187f9d39bd26a5
Amir Keivan Mohtashami <akmohtashami97@gmail.com>
2020-11-12 00:39:19 +0100

    Add half cifar dataset

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,7 +1,9 @@
 import os
 
 import torch
+from torch.utils.data import random_split
 from torchvision import transforms, datasets
+import  numpy as np
 
 
 def get_cifar10_loaders():
@@ -29,12 +31,42 @@ def get_cifar10_loaders():
         shuffle=False,
         **loader_args)
 
-    return {"train_loader": full_train_loader,
+def get_cifar10_half_loaders():
+    normalize_transform = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    train_transform = transforms.Compose([transforms.ToTensor(), normalize_transform])
+    test_transform = transforms.Compose([transforms.ToTensor(), normalize_transform])
+
+    original_train_dataset = datasets.CIFAR10(root=os.path.join('data', 'cifar10_data'),
+                                              train=True, transform=train_transform, download=True)
+    original_test_dataset = datasets.CIFAR10(root=os.path.join('data', 'cifar10_data'),
+                                             train=False, transform=test_transform, download=True)
+
+    dataset_size = len(original_train_dataset)
+    split = int(np.floor(0.5 * dataset_size))
+    half_train_dataset, _ = random_split(original_train_dataset, [dataset_size - split, split])
+
+    loader_args = {
+        "batch_size": 256,
+
+    }
+
+    train_loader = torch.utils.data.DataLoader(
+        dataset=half_train_dataset,
+        shuffle=True,
+        **loader_args)
+
+    test_loader = torch.utils.data.DataLoader(
+        dataset=original_test_dataset,
+        shuffle=False,
+        **loader_args)
+
+    return {"train_loader": train_loader,
             "test_loader": test_loader}
 
 
 dataset_factories = {
-    'cifar10': get_cifar10_loaders
+    'cifar10': get_cifar10_loaders,
+    'half_cifar10': get_cifar10_half_loaders
 }
 
 
