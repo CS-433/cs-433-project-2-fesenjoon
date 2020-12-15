56c37ad730452959807e52383970c4fda10d6427
Amir Keivan Mohtashami <akmohtashami97@gmail.com>
2020-12-15 21:01:51 +0100

    Add learning rate

--- figures/figure9/one_then_another.sh
+++ figures/figure9/one_then_another.sh
@@ -10,8 +10,10 @@ if [ $dataset1 == "cifar100" ]; then
 	num_classes=100
 fi
 
-for dataset_portion in 0.2 0.4 0.6 0.8 1.0; 
+#for dataset_portion in 0.2 0.4 0.6 0.8 1.0;
+for dataset_portion in 0.3 0.6 1.0;
 do
+    """
 	python3 train.py \
 		${dataset1}_${dataset_portion}_data \
 		--exp-dir exp/${dataset1}_${dataset_portion}_data \
@@ -19,21 +21,24 @@ do
 		--lr 0.001 \
 		--optimizer sgd \
 		--dataset-portion $dataset_portion
+	"""
 
 	python3 train.py \
 		${dataset1}_then_${dataset2}_${dataset_portion}_data \
 		--exp-dir exp/${dataset1}_then_${dataset2}_${dataset_portion}_data \
 		--dataset ${dataset2} \
 		--optimizer sgd \
+		--lr 0.001 \
 		--checkpoint-num-classes $num_classes \
 		--checkpoint exp/${dataset1}_${dataset_portion}_data/final.pt \
 		--dataset-portion $dataset_portion
 	
 	python3 train.py \
-		${dataset1}_then_${dataset2}_${dataset_portion}_data_shrink0.3_perturb_0.0001\
+		${dataset1}_then_${dataset2}_${dataset_portion}_data_shrink0.3_perturb_0.0001 \
 		--exp-dir exp/${dataset1}_then_${dataset2}_${dataset_portion}_data_shrink0.3_perturb_0.0001 \
 		--dataset ${dataset2} \
 		--optimizer sgd \
+		--lr 0.001 \
 		--checkpoint-num-classes $num_classes \
 		--checkpoint exp/${dataset1}_${dataset_portion}_data/final.pt \
 		--checkpoint-shrink 0.3 \
@@ -45,6 +50,7 @@ do
 		--exp-dir exp/${dataset1}_then_${dataset3}_${dataset_portion}_data \
 		--dataset ${dataset3} \
 		--optimizer sgd \
+		--lr 0.001 \
 		--checkpoint-num-classes $num_classes \
 		--checkpoint exp/${dataset1}_${dataset_portion}_data/final.pt \
 		--dataset-portion $dataset_portion
@@ -54,6 +60,7 @@ do
 		--exp-dir exp/${dataset1}_then_${dataset3}_${dataset_portion}_data_shrink0.3_perturb_0.0001 \
 		--dataset ${dataset3} \
 		--optimizer sgd \
+		--lr 0.001 \
 		--checkpoint-num-classes $num_classes \
 		--checkpoint exp/${dataset1}_${dataset_portion}_data/final.pt \
 		--checkpoint-shrink 0.3 \
