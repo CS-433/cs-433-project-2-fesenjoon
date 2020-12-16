4fe5e697953b5aaad608403b632f458f69eadb61
Amir Keivan Mohtashami <akmohtashami97@gmail.com>
2020-12-16 21:08:17 +0100

    Fix merge bug

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -189,7 +189,7 @@ def get_cifar10_two_half_loaders(batch_size=128, data_aug=False, drop_last=False
    
 
 
-def get_cifar100_loaders(use_half_train=False, dataset_portion=None):
+def get_cifar100_loaders(use_half_train=False, dataset_portion=None, data_aug=False):
     normalize_transform = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
     train_transform = transforms.Compose([transforms.ToTensor(), normalize_transform])
     test_transform = transforms.Compose([transforms.ToTensor(), normalize_transform])
