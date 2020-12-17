6c4f055e68019ca9d28136e854b681907b08b998
Klim Kireev <klim.kireev@epfl.ch>
2020-12-17 11:08:18 +0100

    Small fix in scripts

--- scripts/figure5_aug.sh
+++ scripts/figure5_aug.sh
@@ -1,3 +1,4 @@
+rm -r exp/grad_track_*
 python3 train.py --dataset first_half_cifar10 --lr 0.001 --epochs 100 grad_track_first_half
 python3 train.py --grad-track  --dataset cifar10 --lr 0.001 --epochs 30 --checkpoint exp/grad_track_first_half/*/final.pt grad_track_ws
 python3 train.py --dataset first_half_cifar10 --lr 0.001 --data-aug --epochs 350 aug_first_half
