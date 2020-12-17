da677da285c3583ab9e429b2f9a7a92cfd1e5d83
Klim Kireev <klim.kireev@epfl.ch>
2020-12-17 12:23:51 +0100

    Data augmentation figure

---
+++ figures/fig_aug.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import matplotlib.pyplot as plt
+import numpy as np
+from utils import get_data_for_runs
+
+runs = {
+    "aug_first/": "DA Warm Start",
+    "aug_second/": "DA Warm Start",
+    "half_cifar/": "Warm Start",
+    "cifar_from_half_pretrained/": "Warm Start",
+}
+
+
+datas, summaries = get_data_for_runs(runs)
+
+offsets = {
+    "DA Warm Start": 350,
+    "Warm Start": 200,
+}
+
+tags = ["train_accuracy", "test_accuracy"]
+names = ["DA Warm Start", "Warm Start"]
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+
+for i in range(2):
+    ax = axes[i]
+    for j, tag in enumerate(tags):
+        x = np.arange(offsets[names[i]] * 2)
+        y = datas[tag][i]
+        y = y * 100
+        ax.plot(x, y, label=tag)
+    ax.plot([offsets[names[i]], offsets[names[i]]], ax.get_ylim(), '--', c='black')
+    ax.legend()
+    ax.set_title(names[i])
+    ax.set(xlabel='Training Epoch', ylabel='Accuracy')
+    ax.set_ylim(0, 100)
+
+plt.savefig("fig_aug.png".format(tag))
---
+++ scripts/fig_aug.sh
@@ -0,0 +1,18 @@
+python3 train.py half_cifar --exp-dir exp/half_cifar/ --dataset half_cifar10 --lr 0.001 --optimizer sgd
+python3 train.py cifar_from_half_pretrained \
+    --exp-dir exp/cifar_from_half_pretrained/ \
+    --dataset cifar10 \
+    --lr 0.001 \
+    --optimizer sgd \
+    --checkpoint exp/half_cifar/final.pt
+
+python3 train.py aug_first --exp-dir exp/aug_first/ --dataset half_cifar10 --lr 0.001 --epochs 350 --data-aug --optimizer sgd
+python3 train.py aug_second \
+    --exp-dir exp/aug_second/ \
+    --dataset cifar10 \
+    --lr 0.001 \
+    --epochs 350 \
+    --data-aug \
+    --optimizer sgd \
+    --checkpoint exp/aug_first/final.pt
+PYTHONPATH=./ python3 figures/fig_aug.py
